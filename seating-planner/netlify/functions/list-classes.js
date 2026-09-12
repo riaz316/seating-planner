0a0876b50ca813a5193754fe5bd101dc407343c7
Mohammad Riaz <mdriaz0407@gmail.com>
2026-09-12 13:56:59 +0100

    Delete seating-planner directory

--- seating-planner/netlify/functions/list-classes.js
+++
@@ -1,9 +0,0 @@
-import { getStore } from "@netlify/blobs";
-
-export default async () => {
-  const store = getStore("seating-planner");
-  const index = (await store.get("classplan-index", { type: "json" })) || [];
-  return new Response(JSON.stringify(index), {
-    headers: { "content-type": "application/json" },
-  });
-};

e769ba458839669649b01ccaabb9c66ce9b64571
Mohammad Riaz <mdriaz0407@gmail.com>
2026-09-12 14:02:20 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -496,7 +496,7 @@ export default function App() {
         }
       `}</style>
 
-      <div className="max-w-7xl mx-auto p-4 sm:p-6">
+      <div className="w-full p-4 sm:p-6">
         <Header
           selectedSlug={selectedSlug}
           view={view}
